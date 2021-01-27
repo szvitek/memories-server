0a53e505366ff80789710537482dc19dca6f4a74
gy.szvitek <gy.szvitek@gmail.com>
2021-01-27 23:57:03 +0100

    feat(posts): delete post route and controller implemented

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getPosts, createPost, updatePost } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost, deletePost } from '../controllers/posts.js';
 
 const router = Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
+router.delete('/:id', deletePost)
 
 export default router;
